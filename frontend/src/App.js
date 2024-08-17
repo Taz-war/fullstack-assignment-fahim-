46a21d0a2f352a039a3fcac0d0867f1f5cb0380f
Tazwar <142654711+Fahim-instaWebWorks@users.noreply.github.com>
2024-08-17 12:42:24 +0600

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className=' font-serif font-bold text-red-400 text-3xl'>Hello from fahim</h1>
+    </div>
+  );
+}
+
+export default App;
